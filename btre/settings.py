505dcc9704e4a81a961100742e07b93057c0298b
mnjayswal84 <mnjayswal84@gmail.com>
2021-02-04 14:23:29 +0530

    Text commit

--- btre/settings.py
+++ btre/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 # Email Config
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mnjayswal10@gmail.com'
-EMAIL_HOST_PASSWORD = '9426523459aA@'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -32,8 +32,8 @@ def contact(request):
             'Property Listing Inquiry',
             'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info.\n\n'+ 
             'The person has send the message for anquiry as follow: '+ message,
-            'mnjayswal10@gmail.com',
-            [realtor_email, 'mihirnjayswal84@gmail.com', 'jmj1482000@gmail.com'],
+            'email',
+            [realtor_email],
             fail_silently=False 
         )
 
